cdb0103b809c74be14cc553be88701b083c61cba
leela Sagar <sagarsean007@gmail.com>
2022-12-30 14:24:35 +0530

    added gitingore and made changes in config.py

--- src/config.py
+++ src/config.py
@@ -1,8 +1,9 @@
 import os
 
-train_data_path = os.path.abspath("../Data/Training_Batch_Files/")
-pred_data_path = os.path.abspath("../Data/Prediction_Batch_Files/")
-train_schema_file = os.path.abspath("../Data/schema_training.json")
-pred_schema_file = os.path.abspath("../Data/schema_prediction.json")
-model_save_location = os.path.abspath("../models")
-output_data_path = os.path.abspath("../Data/Output")
+train_data_path = os.path.abspath("Data/Training_Batch_Files/")
+pred_data_path = os.path.abspath("Data/Prediction_Batch_Files/")
+train_schema_file = os.path.abspath("Data/schema_training.json")
+pred_schema_file = os.path.abspath("Data/schema_prediction.json")
+model_save_location = os.path.abspath("models")
+output_data_path = os.path.abspath("Data/Output")
+
