0add864f8c905f4706dbdb65335b13fbf15140d3
leela Sagar <sagarsean007@gmail.com>
2022-12-30 17:46:00 +0530

    added argparser in run.py

--- src/run.py
+++ src/run.py
@@ -1,8 +1,19 @@
 from training_model import Train_model
 from prediction_model import Prediction
+import argparse
 
-# train_model = Train_model()
-# train_model.train()
+parser = argparse.ArgumentParser()
+parser.add_argument('--action',choices=['train','predict'], type=str,required=True)
+args = parser.parse_args()
 
-prediction_model = Prediction()
-prediction_model.prediction_from_model()
+if args.action.lower() in ['train','training'] :
+
+    train_model = Train_model()
+    train_model.train()
+elif args.action.lower() in ['predict','prediction']:
+    prediction_model = Prediction()
+    prediction_model.prediction_from_model()
+else:
+    print("wrong argumnets")
+
+
